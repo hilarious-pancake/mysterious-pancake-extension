70736a8e8eba9d29cd751e06689746659a37d47d
JasonLy <jason.ly@gmail.com>
2015-03-26 00:34:03 -0700

    Implement hello world with react - it appears that there are some problems with the way the scripts are injected using chrome extensions.

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,11 +6,11 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+// chrome.extension.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+//   });
 
 // $('p').on('click', function(){
 //   alert('hello');
