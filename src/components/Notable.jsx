e8d10e8a58f05ff91c92ffdb5ffdfc50173d082e
Conor Flannigan <cflanni1@jhu.edu>
2015-03-26 22:44:16 -0700

    Underline annotated components
    Also: fix highlighting. There were errors with the toggle status getting
    arbitrarily switched for some reason.

--- src/components/Notable.jsx
+++ src/components/Notable.jsx
@@ -1,9 +1,30 @@
 var $ = require('jquery');
 var React = require('react');
 var Annotation = require('./Annotation.jsx')
+var Firebase = require('firebase');
+var ReactFireMixin = require('reactfire');
+var sha1 = require('sha1');
 
 //component for notable objects
 var Notable = React.createClass({
+
+  mixins: [ReactFireMixin],
+
+  getInitialState: function() {
+    return {
+      notes: []
+    };
+  },
+
+  componentWillMount: function() {
+    // hash the url to get a unique storage id for the webpage
+    var docHash = sha1(window.location.href);
+    // use the text as hashed ID for firebase
+    var hash = sha1(this.props.text);
+    this.bindAsArray(new Firebase(
+      "https://popping-torch-5999.firebaseio.com/docs/"+docHash+"/notables/"+hash+"/notes"),
+      "notes");
+  },
 
   componentDidMount: function() {
     $(React.findDOMNode(this.refs.conor)).css({
@@ -28,14 +49,21 @@ var Notable = React.createClass({
     }
   },
 
-  toggleHighlight: function(e) {
-    $(React.findDOMNode(this.refs.jason)).toggleClass('mgnl-highlight');
+  highlightOn: function(e) {
+    $(React.findDOMNode(this.refs.jason)).addClass('mgnl-highlight');
+  },
+
+  highlightOff: function(e) {
+    $(React.findDOMNode(this.refs.jason)).removeClass('mgnl-highlight');
   },
 
   render: function(){
 
+    var hasNotes = this.state.notes.length > 0;
+    var classString = (hasNotes) ? "jason mgnl-underline" : "jason";
+
     return (
-      <span className="jason" ref="jason" onClick={this.annotationPopUp} onMouseEnter={this.toggleHighlight} onMouseLeave={this.toggleHighlight}>
+      <span className={classString} ref="jason" onClick={this.annotationPopUp} onMouseEnter={this.highlightOn} onMouseLeave={this.highlightOff}>
         {this.props.text}
         <Annotation ref="conor" parentText={this.props.text}>
         </Annotation>
