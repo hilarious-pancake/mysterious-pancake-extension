accce4b29c48df576f74e528d4dbd582305872c5
Conor Flannigan <cflanni1@jhu.edu>
2015-03-26 19:47:22 -0700

    Sync new annotations with firebase

--- src/components/Annotation.jsx
+++ src/components/Annotation.jsx
@@ -1,6 +1,8 @@
+var $ = require('jquery');
 var React = require('react');
 var Firebase = require('firebase');
 var ReactFireMixin = require('reactfire');
+var sha1 = require('sha1');
 
 //component for annotations
 var Annotation = React.createClass({
@@ -8,7 +10,14 @@ var Annotation = React.createClass({
   mixins: [ReactFireMixin],
 
   componentWillMount: function() {
-    this.bindAsArray(new Firebase("https://popping-torch-5999.firebaseio.com/???/notes"), "notes");
+    // hash the url to get a unique storage id for the webpage
+    var docHash = sha1(window.location.href);
+    // use the text from the parent as the hashed ID for the
+    // notable stored in firebase
+    var parentHash = sha1(this.props.parentText);
+    this.bindAsArray(new Firebase(
+      "https://popping-torch-5999.firebaseio.com/docs/"+docHash+"/notables/"+parentHash+"/notes"),
+      "notes");
   },
 
   componentDidMount: function() {
@@ -17,17 +26,19 @@ var Annotation = React.createClass({
 
   addAnnotation: function() {
     var text = $(React.findDOMNode(this.refs.input)).val();
-    // TODO: sync to firebase
+    this.firebaseRefs["notes"].push({
+      text: text
+    });
   },
 
   render: function(){
     var style = {
       position: "absolute",
-      "background-color": "#cccccc",
-      "width": "400px",
-      "height": "200px",
-      "border-radius": "5px",
-      "opacity": "0.5",
+      backgroundColor: "#cccccc",
+      width: "400px",
+      height: "200px",
+      borderRadius: "5px",
+      opacity: "0.5",
       padding: "5px" 
       // top: this.props.top,
       // left: this.props.left
@@ -39,7 +50,7 @@ var Annotation = React.createClass({
         <ul className="mrgn-list">
         </ul>
         <input ref="input"/>
-        <button onClick="this.addAnnotation">Comment</button>
+        <button onClick={this.addAnnotation}>Comment</button>
       </div>
     )
   }
